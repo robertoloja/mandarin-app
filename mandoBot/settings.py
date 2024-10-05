68061bfa4f2bce9df10bda9585f3457385c5fa7e
Roberto Loja <robertoloja@gmail.com>
2024-10-05 19:43:59 -0400

    SCRUM-11 Configured CORS

--- mandoBot/settings.py
+++ mandoBot/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["mandobot.pythonanywhere.com"]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://robertoloja.github.io",
+]
+
 
 # Application definition
 
@@ -38,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local apps
+    'corsheaders',
     'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
