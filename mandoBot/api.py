a111ef2895a073f6667d96a7afe74c2ea7f8f746
Roberto Loja <robertoloja@gmail.com>
2024-12-19 14:34:51 -0500

    segment endpoint added with test values

--- mandoBot/api.py
+++ mandoBot/api.py
@@ -2,7 +2,7 @@ from ninja import NinjaAPI
 from typing import List
 
 from accounts.models import CustomUser
-from .schemas import UserSchema, CEDictionary, CEDictSchema, WordSchema
+from .schemas import UserSchema, CEDictionary, CEDictSchema, WordSchema, SegmentationResponse
 
 api = NinjaAPI()
 
@@ -13,3 +13,25 @@ def get_users(request):
 @api.post("/dictionary", response=List[CEDictSchema])
 def dictionary(request, data: WordSchema):
   return list(CEDictionary.objects.filter(traditional=data.word))
+
+@api.post("/segment", response=SegmentationResponse)
+def segment(request, data: str):
+  return {
+    "translation": "test translation",
+    "dictionary": {
+      "traditional": "這是一個測試句",
+      "simplified": "这是一个测试句子",
+      "pronunciation": "zhè shì yīgè cèshì jù",
+      "definitions": ["this", "is", "a", "test", "sentence"]
+    },
+    "sentence": [{
+      "word": "這",
+      "pinyin": "zhè",
+      "definitions": ["this"], 
+      "dictionary": {
+        "english": "this",
+        "pinyin": "zhè",
+        "simplified": "这"
+      }
+    }]
+  }
--- mandoBot/schemas.py
+++ mandoBot/schemas.py
@@ -1,6 +1,18 @@
 
+from typing import List
 from ninja import Schema, ModelSchema
 from sentences.models import CEDictionary
+
+class ChineseDictionary(Schema):
+  english: str
+  pinyin: str
+  simplified: str
+
+class MandarinWordSchema(Schema):
+  word: str
+  pinyin: str
+  definitions: List[str]
+  dictionary: ChineseDictionary
 
 class UserSchema(Schema):
   email: str
@@ -12,3 +24,8 @@ class CEDictSchema(ModelSchema):
   class Meta:
     model = CEDictionary
     fields = ['traditional', 'simplified', 'pronunciation', 'definitions']
+
+class SegmentationResponse(Schema):
+  translation: str
+  dictionary: dict
+  sentence: List[MandarinWordSchema]
