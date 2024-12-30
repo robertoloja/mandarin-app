7f90bba3fb4e23196931b6f51c09403a36072562
Roberto Loja <robertoloja@gmail.com>
2024-12-30 17:48:59 -0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/mandarin-app',
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"]
   }
