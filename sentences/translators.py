b95581203527b236349bef39e9bb2c128ceeeeb6
Roberto Loja <robertoloja@gmail.com>
2024-12-30 20:02:06 -0500

    Trying DeepL translation API

--- sentences/translators.py
+++ sentences/translators.py
@@ -1,33 +1,42 @@
-import argostranslate.package
-import argostranslate.translate
-from dragonmapper import hanzi
+# import argostranslate.package
+# import argostranslate.translate
+# from dragonmapper import hanzi
+import deepl
+import os
 
-# Download and install Argos Translate package. Only runs on server bootup. 
-english_code = "en"
-traditional_mandarin_code = "zt"
-simplified_mandarin_code = "zh"
-
-argostranslate.package.update_package_index()
-available_packages = argostranslate.package.get_available_packages()
-traditional_mandarin = next(filter(lambda x: x.from_code == traditional_mandarin_code and
-                                          x.to_code == english_code, available_packages))
-
-simplified_mandarin = next(filter(lambda x: x.from_code == simplified_mandarin_code and
-                                         x.to_code == english_code, available_packages))
-
-argostranslate.package.install_from_path(traditional_mandarin.download())
-argostranslate.package.install_from_path(simplified_mandarin.download())
-
-class ArgosTranslate:
+translator = deepl.Translator(os.getenv('DEEPL_API_KEY'))
+class DeepLTranslate:
     @staticmethod
     def translate(sentence: str) -> str:
-        from_code = simplified_mandarin_code
+        return translator.translate_text(sentence, source_lang='ZH', target_lang='EN-US').text
 
-        if hanzi.is_traditional(sentence):
-            from_code = traditional_mandarin_code
 
-        return argostranslate.translate.translate(sentence, 
-                                                  from_code, 
-                                                  english_code)
+# Download and install Argos Translate package. Only runs on server bootup. 
+# english_code = "en"
+# traditional_mandarin_code = "zt"
+# simplified_mandarin_code = "zh"
 
-DefaultTranslator = ArgosTranslate
+# argostranslate.package.update_package_index()
+# available_packages = argostranslate.package.get_available_packages()
+# traditional_mandarin = next(filter(lambda x: x.from_code == traditional_mandarin_code and
+#                                           x.to_code == english_code, available_packages))
+
+# simplified_mandarin = next(filter(lambda x: x.from_code == simplified_mandarin_code and
+#                                          x.to_code == english_code, available_packages))
+
+# argostranslate.package.install_from_path(traditional_mandarin.download())
+# argostranslate.package.install_from_path(simplified_mandarin.download())
+
+# class ArgosTranslate:
+#     @staticmethod
+#     def translate(sentence: str) -> str:
+#         from_code = simplified_mandarin_code
+
+#         if hanzi.is_traditional(sentence):
+#             from_code = traditional_mandarin_code
+
+#         return argostranslate.translate.translate(sentence, 
+#                                                   from_code, 
+#                                                   english_code)
+
+DefaultTranslator = DeepLTranslate
