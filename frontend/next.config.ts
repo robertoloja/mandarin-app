0a4f09c3fe0fc905af206fb82a9a3d88e5deaa92
Roberto Loja <robertoloja@gmail.com>
2024-12-31 00:40:12 -0500

    Remote deployment options for github pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // basePath: '/mandarin-app',
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   },
