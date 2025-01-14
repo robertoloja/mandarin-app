d9900e752b22be43a9f0458b45b158a6200dcd18
Roberto Loja <robertoloja@gmail.com>
2025-01-14 05:32:46 -0500

    Added the project support box

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,223 +1,145 @@
 'use client';
-import { MandoBotAPI } from '@/utils/api';
-import { clearError, setError } from '@/utils/store/errorSlice';
-import { store } from '@/utils/store/store';
+
+import ServerStatusComponent from '@/components/ServerStatusComponent';
 import {
-  Center,
-  Heading,
-  Image,
-  Text,
-  VStack,
-  Flex,
   Box,
-  useColorMode,
+  Center,
+  ColorModeContextType,
+  Flex,
+  Heading,
   HStack,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverArrow,
-  PopoverBody,
+  ListItem,
+  OrderedList,
+  Spacer,
+  Text,
+  UnorderedList,
+  useColorMode,
+  VStack,
 } from '@chakra-ui/react';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import {
-  IoAlertCircleOutline,
-  IoCheckmarkCircleOutline,
-  IoInformationCircleOutline,
-} from 'react-icons/io5';
 
 export default function AboutPage() {
-  return <ServerStatusComponent />;
-}
-
-const ServerStatusComponent = () => {
   const { colorMode } = useColorMode();
-  const [localDateTime, setLastUpdate] = useState('-');
-  const [translationBackend, setBackend] = useState('-');
-  const [responseTime, setResponseTime] = useState(-1);
-  const [serverStatus, setServerStatus] = useState(false);
-
-  useEffect(() => {
-    MandoBotAPI.status()
-      .then((response) => {
-        store.dispatch(clearError());
-        const isoUpdateTime = new Date(response.updated_at);
-        const localTime = isoUpdateTime.toLocaleTimeString(undefined, {
-          year: 'numeric',
-          month: 'numeric',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-        });
-        setLastUpdate(`${localTime}`);
-        setResponseTime(response.average_response_time);
-        setBackend(response.translation_backend);
-        setServerStatus(true);
-      })
-      .catch((error) => {
-        setServerStatus(false);
-        store.dispatch(
-          setError('Server currently unreachable. Please try again soon.'),
-        );
-      });
-  }, []);
   return (
-    <Box mt={6} justifyContent="center">
-      <HStack justifyContent="center">
-        <Heading size="md" textAlign="center" mb={1}>
-          Server Status
-        </Heading>
-        <ServerStatusPopover serverStatus={serverStatus} />
-      </HStack>
-      <Box display="flex" justifyContent="center">
-        <Flex
-          direction={{ base: 'column', md: 'row' }}
-          align="center"
-          justify="center"
-          p={4}
-          m={[2, 10]}
-          gap={4}
-          width="fit-content"
-          border={
-            colorMode === 'light' ? '1px solid #468DA4' : '1px solid #1e282c'
-          }
-          borderRadius={8}
-          boxShadow="1px 1px 2px rgba(0, 0, 0, 0.5)"
-          backgroundColor={colorMode === 'light' ? '#B8EEFF' : '#333c40'}
-        >
-          <Box
-            m={2}
-            border={
-              colorMode === 'light' ? '1px solid #468DA4' : '1px solid #1e282c'
-            }
-            p={5}
-            borderRadius={8}
-            boxShadow="1px 1px 1px rgba(0, 0, 0, 0.3)"
-          >
-            <VStack>
-              <Heading size="sm">Average First Response Time</Heading>
-              <Text fontSize="lg">{responseTime / 1000} seconds</Text>
-              <Text fontSize="sm">Last Update: {localDateTime}</Text>
-            </VStack>
-          </Box>
-
-          <Box
-            m={2}
-            border={
-              colorMode === 'light' ? '1px solid #468DA4' : '1px solid #1e282c'
-            }
-            p={5}
-            borderRadius={8}
-            boxShadow="1px 1px 1px rgba(0, 0, 0, 0.3)"
-          >
-            <VStack mb={3} mx={2}>
-              <HStack>
-                <Heading size="sm">Current Translation Backend</Heading>
-                {serverStatus && translationBackend == 'argos' && (
-                  <InformationPopover />
-                )}
-              </HStack>
-              {serverStatus ? (
-                <TranslationBackendComponent shortName={translationBackend} />
-              ) : (
-                <Text>-</Text>
-              )}
-            </VStack>
-          </Box>
-        </Flex>
-      </Box>
+    <Box
+      display="flex"
+      justifyContent="center"
+      width="100vw"
+      overflowX="hidden"
+    >
+      <Flex
+        justifyContent="center"
+        direction={{ base: 'column', lg: 'row' }}
+        align="center"
+        alignItems={{ base: 'center', lg: 'flex-start' }}
+        justify="center"
+        p={4}
+        m={[2, 10]}
+        gap={4}
+        maxWidth="100vw"
+        boxSizing="border-box"
+      >
+        <ServerStatusComponent />
+        <ProjectSupportCard />
+      </Flex>
     </Box>
   );
-};
+}
 
-const ServerStatusPopover = (props: { serverStatus: boolean }) => {
+const ProjectSupportCard = () => {
+  const { colorMode } = useColorMode();
   return (
-    <>
-      {props.serverStatus ? (
-        <Popover>
-          <PopoverTrigger>
-            <IoCheckmarkCircleOutline
-              color="green"
-              size={22}
-              cursor="pointer"
-            />
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverBody boxShadow="1px 1px 2px rgba(0, 0, 0, 0.8)">
-              <Center>Everything is working!</Center>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      ) : (
-        <Popover>
-          <PopoverTrigger>
-            <IoAlertCircleOutline color="red" size={22} cursor="pointer" />
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverBody boxShadow="1px 1px 2px rgba(0, 0, 0, 0.8)">
-              <Center>
-                The segmentation server is currently unreachable. Please check
-                your internet connection, and try again soon.{' '}
-              </Center>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      )}
-    </>
-  );
-};
+    <Box>
+      <Heading size="md" textAlign="center" mb={4} whiteSpace="nowrap">
+        Support This Project
+      </Heading>
 
-const InformationPopover = () => {
-  return (
-    <Popover>
-      <PopoverTrigger>
-        <IoInformationCircleOutline size={22} cursor="pointer" />
-      </PopoverTrigger>
-      <PopoverContent>
-        <PopoverArrow />
-        <PopoverBody boxShadow="1px 1px 2px rgba(0, 0, 0, 0.5)">
-          <Text mb={3}>
-            While Argos Translate is a very good open-source project, it can
-            sometimes make egregious mistakes when translating Mandarin (such as
-            rendering "京" as "Kyoto").
-          </Text>
-          <Text>
-            Users at the "Project Backer" level always have access to{' '}
-            <Link href="https://www.deepl.com">
-              <u>DeepL</u>
-            </Link>
-            , the best publicly available translation engine.
-          </Text>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
-  );
-};
-
-const TranslationBackendComponent = (props: { shortName: string }) => {
-  return (
-    <Link
-      href={
-        props.shortName === 'deepl'
-          ? 'https://www.deepl.com'
-          : 'https://www.argosopentech.com/'
-      }
-    >
-      <Center>
-        <Image
-          src={
-            props.shortName === 'deepl'
-              ? '/deepl_logo.svg'
-              : 'argos_translate_logo.png'
-          }
-          boxSize={10}
-        />
-        <Text ml={2} _hover={{ textDecoration: 'underline' }}>
-          {props.shortName == 'deepl' ? 'DeepL' : 'Argos Translate'}
+      <Box
+        justifyContent="center"
+        w="fit-content"
+        m={2}
+        border={
+          colorMode === 'light' ? '1px solid #468DA4' : '1px solid #1e282c'
+        }
+        p={5}
+        borderRadius={8}
+        backgroundColor={colorMode === 'light' ? '#B8EEFF' : '#333c40'}
+        boxShadow="1px 1px 1px rgba(0, 0, 0, 0.3)"
+        minWidth="30vw"
+      >
+        <Text mb={4}>
+          If this website is useful to you, please consider supporting the
+          project with a monthly subscription. Doing so provides several
+          benefits:
         </Text>
-      </Center>
-    </Link>
+
+        <Center>
+          <VStack>
+            <HStack>
+              <Heading size="sm">$5 / month</Heading>
+            </HStack>
+            <Box
+              p={3}
+              mb={5}
+              borderRadius={8}
+              boxShadow="1px 1px 2px rgba(0, 0, 0, 0.5)"
+              backgroundColor={colorMode === 'light' ? '#85E2FF' : '#495255'}
+              border={
+                colorMode === 'light'
+                  ? '1px solid #468DA4'
+                  : '1px solid #1e282c'
+              }
+            >
+              <OrderedList p={4}>
+                <ListItem>
+                  <b>Increased Segmentation Limit</b>
+                </ListItem>
+                <UnorderedList mb={3}>
+                  <ListItem>
+                    Project supporters can segment sentences of up to 1,000
+                    characters
+                  </ListItem>
+                  <ListItem>Free users are limited to 200 characters</ListItem>
+                </UnorderedList>
+
+                <ListItem>
+                  <b>Always the best available translation</b>
+                </ListItem>
+                <UnorderedList mb={3}>
+                  <ListItem>
+                    Project supporter translations always use DeepL, the
+                    best-in-class translation system
+                  </ListItem>
+                  <ListItem>
+                    Free users' translations are performed by Argos Translate
+                  </ListItem>
+                </UnorderedList>
+
+                <ListItem>
+                  <b>
+                    Advance Access to Reading Room and Prioritized Suggestions
+                  </b>
+                </ListItem>
+                <UnorderedList>
+                  <ListItem>
+                    Project Supporters receive access to chapters as soon as
+                    they are created, and can request specific texts to be added
+                  </ListItem>
+                  <ListItem>
+                    Free users access new texts once all chapters are finished
+                  </ListItem>
+                </UnorderedList>
+              </OrderedList>
+            </Box>
+          </VStack>
+        </Center>
+        <Text mb={2}>
+          MandoBot is built and maintained by a single developer on a shoestring
+          budget. While every optimization is used to maintain good performance,
+          free hosting can only go so far. If you experience any issues at all,
+          please <u>contact me</u>, or <u>fill out a bug report</u>, and it will
+          be addressed as soon as possible.
+        </Text>
+      </Box>
+    </Box>
   );
 };
