3bbaaf6df648ab58ef906d9a17903d82312461c3
Roberto Loja <robertoloja@gmail.com>
2025-01-19 17:03:16 -0500

    Migration for decimals in server status

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import { RootState } from '@/utils/store/store';
+import { useSelector } from 'react-redux';
+import { Box, Container, Heading, Text } from '@chakra-ui/react';
+
 export default function Settings() {
-  return <>settings</>;
+  const user = useSelector((state: RootState) => state.auth.user);
+  const email = useSelector((state: RootState) => state.auth.email);
+  const theme = useSelector((state: RootState) => state.settings.theme);
+  const pronunciation = useSelector(
+    (state: RootState) => state.settings.pronunciation,
+  );
+  return (
+    <Box border="1px solid #AAA">
+      <Heading>Settings</Heading>
+      <Box></Box>
+    </Box>
+  );
 }
