ab0415879aa8263b8c113b036e88eacbff8a71c5
Roberto Loja <robertoloja@gmail.com>
2026-04-14 13:05:31 +0200

    Frontend: Fix formatting

--- frontend/src/localization/about_status.ts
+++ frontend/src/localization/about_status.ts
@@ -16,23 +16,23 @@ const about_status = {
     },
     content: {
       intro: {
-        en: "By using this app, you agree to these Terms of Service. Please read them carefully before using the service.",
+        en: 'By using this app, you agree to these Terms of Service. Please read them carefully before using the service.',
         de: 'Mit der Nutzung dieser Anwendung akzeptieren Sie die Nutzungsbedingungen vollständig. Bitte lesen Sie diese sorgfältig, bevor Sie unseren Service in Anspruch nehmen.',
       },
       1: {
-        en: "Use of the Service: You may use the app only for lawful purposes and in accordance with these Terms. By creating an account, you confirm that: You are at least 13 years old. You will not misuse the app to upload harmful, illegal, or offensive content.",
+        en: 'Use of the Service: You may use the app only for lawful purposes and in accordance with these Terms. By creating an account, you confirm that: You are at least 13 years old. You will not misuse the app to upload harmful, illegal, or offensive content.',
         de: 'Nutzung dieses Services: Diese Anwendung darf ausschließlich für legale und erlaubte Zwecke und in Übereinstimmung mit den Nutzungsbedingungen verwendet werden. Durch die Erstellung eines Nutzerkontos versichern Sie, dass: Sie mindestens 13 Jahre alt sind; Sie die Anwendung nicht missbrauchen, um schädliche, rechtswidrige oder anderweitig anstößige Inhalte hochzuladen.',
       },
       2: {
-        en: "User-Generated Content Ownership: You retain ownership of the Mandarin sentences you input into the app. Deletion: You may delete your content at any time via the app interface. Responsibility: You are solely responsible for the content you upload and its legality.",
+        en: 'User-Generated Content Ownership: You retain ownership of the Mandarin sentences you input into the app. Deletion: You may delete your content at any time via the app interface. Responsibility: You are solely responsible for the content you upload and its legality.',
         de: 'Eigentumsrecht an nutzererstellten Inhalten: Die Inhalte in Mandarin, die Sie in die Anwendung eingeben, bleiben stets Ihr Eigentum. Löschung: Ihre Inhalte können jederzeit innerhalb der App von Ihnen gelöscht werden. Verantwortung: Sie tragen die Verantwortung für von Ihnen hochgeladenen Inhalte und deren Legalität, sowie für Ihre Aktivitäten in der App.',
       },
       3: {
-        en: "Account Responsibility: You are responsible for maintaining the confidentiality of your account credentials. You agree to notify us immediately of any unauthorized use of your account.",
+        en: 'Account Responsibility: You are responsible for maintaining the confidentiality of your account credentials. You agree to notify us immediately of any unauthorized use of your account.',
         de: 'Verantwortung für Nutzerkonto: Sie sind für die Sicherheit ihres Nutzerkontos verantwortlich und versichern, Ihre Kontoinformationen vertraulich zu behandeln. Sie stimmen zu, uns im Falle von unbefugter Verwendung ihres Kontos umgehend zu benachrichtigen.',
       },
       4: {
-        en: "Data Storage and Privacy: By using the app, you consent to the storage of your email and user-generated content as outlined in our Privacy Policy. We do not claim ownership of your data but reserve the right to remove content that violates these Terms.",
+        en: 'Data Storage and Privacy: By using the app, you consent to the storage of your email and user-generated content as outlined in our Privacy Policy. We do not claim ownership of your data but reserve the right to remove content that violates these Terms.',
         de: 'Datenspeicherung und Privatsphäre: Durch die Nutzung dieser Anwendung stimmen Sie zu, dass Ihre E-Mail Adresse und hochgeladene Inhalte wie in unserer Datenschutzerklärung beschrieben gespeichert werden. Wir erheben keinerlei Besitzansprüche auf Ihre Daten, behalten uns jedoch das Recht vor, Inhalte zu löschen, wenn diese gegen unsere Nutzungsbedingungen verstoßen.',
       },
       5: {
@@ -40,15 +40,15 @@ const about_status = {
         de: 'Haftungsausschuss: mandoBot wird ohne Gewähr und Anspruch auf Vollständigkeit bereitgestellt. Wir bemühen uns um Aktualität und Vollständigkeit der Anwendung, gewährleisten jedoch nicht, dass unser Service stets korrekt, abrufbar oder für Ihre Zwecke passend ist. Soweit gesetzlich zulässig schließen wir die Haftung für jegliche Schäden aus, welche aus der Nutzung unserer Anwendung resultieren.',
       },
       6: {
-        en: "Termination: We reserve the right to suspend or terminate your account if you violate these Terms.",
+        en: 'Termination: We reserve the right to suspend or terminate your account if you violate these Terms.',
         de: 'Kündigung von Konten: Wir behalten uns vor, Ihr Konto zu sperren oder zu löschen, falls Sie gegen die hier vorliegenden Bedingungen verstoßen.',
       },
       7: {
-        en: "Changes to These: Terms We may update these Terms from time to time. Significant changes will be communicated via email or through the app.",
+        en: 'Changes to These: Terms We may update these Terms from time to time. Significant changes will be communicated via email or through the app.',
         de: 'Änderungen der Nutzungsbedingungen: Es ist möglich, dass wir diese Bedingungen mitunter aktualisieren. Erhebliche Änderungen werden über E-Mail oder durch die App mitgeteilt.',
       },
       8: {
-        en: "Contact: For questions or concerns about these Terms, contact us at mandobotserver@gmail.com",
+        en: 'Contact: For questions or concerns about these Terms, contact us at mandobotserver@gmail.com',
         de: 'Kontakt: Bei Fragen oder Anliegen zu diesen Bedingungen kontaktieren Sie uns bitte unter mandobotserver@gmail.com',
       },
     },
@@ -60,35 +60,37 @@ const about_status = {
     },
     content: {
       intro: {
-        en: "If mandoBot is useful to you, please consider supporting the project with a monthly subscription. Doing so provides several benefits:",
+        en: 'If mandoBot is useful to you, please consider supporting the project with a monthly subscription. Doing so provides several benefits:',
         de: 'Wenn mandoBot hilfreich für Sie ist, ziehen Sie doch in Erwägung, dieses Projekt mit einem monatlichen Abo zu unterstützen. Dies hat einige Vorteile:',
+      },
     },
-  },
-  server_status: {
     server_status: {
-      en: 'Server Status',
-      de: 'Server Status',
-    },
-    response_time: {
-      en: 'Average First Response Time',
-      de: 'Durchschnittliche erste Response-Zeit',
-    },
-    seconds: {
-      en: 'seconds',
-      de: 'Sekunden',
-    },
-    last_update: {
-      en: 'Last Update',
-      de: 'Letztes Update',
-    },
-    backend: {
-      en: 'Current Translation Backend',
-      de: 'Derzeitiges Übersetzungs-Backend',
-    },
-    checkmark: {
-      en: 'Everything is working!',
-      de: 'Alles funktioniert!',
+      server_status: {
+        en: 'Server Status',
+        de: 'Server Status',
+      },
+      response_time: {
+        en: 'Average First Response Time',
+        de: 'Durchschnittliche erste Response-Zeit',
+      },
+      seconds: {
+        en: 'seconds',
+        de: 'Sekunden',
+      },
+      last_update: {
+        en: 'Last Update',
+        de: 'Letztes Update',
+      },
+      backend: {
+        en: 'Current Translation Backend',
+        de: 'Derzeitiges Übersetzungs-Backend',
+      },
+      checkmark: {
+        en: 'Everything is working!',
+        de: 'Alles funktioniert!',
+      },
     },
   },
 };
+
 export default about_status;
