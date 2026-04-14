e93b01e83c8188bbb7a0cc6dfd4493e370333201
Roberto Loja <robertoloja@gmail.com>
2026-04-14 13:08:04 +0200

    Frontend: Fix formatting

--- frontend/src/localization/about_status.ts
+++ frontend/src/localization/about_status.ts
@@ -64,31 +64,31 @@ const about_status = {
         de: 'Wenn mandoBot hilfreich für Sie ist, ziehen Sie doch in Erwägung, dieses Projekt mit einem monatlichen Abo zu unterstützen. Dies hat einige Vorteile:',
       },
     },
+  },
+  server_status: {
     server_status: {
-      server_status: {
-        en: 'Server Status',
-        de: 'Server Status',
-      },
-      response_time: {
-        en: 'Average First Response Time',
-        de: 'Durchschnittliche erste Response-Zeit',
-      },
-      seconds: {
-        en: 'seconds',
-        de: 'Sekunden',
-      },
-      last_update: {
-        en: 'Last Update',
-        de: 'Letztes Update',
-      },
-      backend: {
-        en: 'Current Translation Backend',
-        de: 'Derzeitiges Übersetzungs-Backend',
-      },
-      checkmark: {
-        en: 'Everything is working!',
-        de: 'Alles funktioniert!',
-      },
+      en: 'Server Status',
+      de: 'Server Status',
+    },
+    response_time: {
+      en: 'Average First Response Time',
+      de: 'Durchschnittliche erste Response-Zeit',
+    },
+    seconds: {
+      en: 'seconds',
+      de: 'Sekunden',
+    },
+    last_update: {
+      en: 'Last Update',
+      de: 'Letztes Update',
+    },
+    backend: {
+      en: 'Current Translation Backend',
+      de: 'Derzeitiges Übersetzungs-Backend',
+    },
+    checkmark: {
+      en: 'Everything is working!',
+      de: 'Alles funktioniert!',
     },
   },
 };
