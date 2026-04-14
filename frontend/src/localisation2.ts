501d6924c79c6d473ee6b525e453fb8eb8fd7d36
keatsgirl <svenjamg95@googlemail.com>
2026-04-14 10:57:31 +0200

    Update and rename localisation2.txt to localisation2.ts
    fixed spacing and added variable definition

--- frontend/src/localisation2.ts
+++ frontend/src/localisation2.ts
@@ -1,37 +1,38 @@
-top_nav: 	
-	en: {
-            text_options: "Text Options",
-            pronunciation: "Pronunciation",
-            off: "off",
-            on: "on",
-            size: "size",
-            definitions: "Definitions",
+const localization2 = {
+	top_nav: {
+		en: {
+			text_options: "Text Options",
+			pronunciation: "Pronunciation",
+			off: "off",
+			on: "on",
+			size: "size",
+			definitions: "Definitions",
 	    language_options: "Language Options",
-        },
-        de: {
-            text_options: "Texteinstellungen",
+		},
+		de: {
+			text_options: "Texteinstellungen",
 	    pronunciation: "Aussprache",
 	    off: "aus",
 	    on: "an",
 	    size: "Größe",
 	    definitions: "Definitionen",
 	    language_options: "Spracheinstellungen",
-        },
-    },
-home_page:
-	en: {
+		},
+	},
+	home_page: {
+		en: {
 	    input_field: "Enter Mandarin text to translate and segment",
 	    submit: "Submit",
 	    info: "Unregistered users are limited to 200 characters per usage. More info.",
-	},
-        de: {
+		},
+    de: {
 	    input_field: "Text in Mandarin zur Übersetzung oder Segmentierung eingeben",
 	    submit: "Absenden",
 	    info: "Nicht registrierte Nutzer sind auf 200 Zeichen per Nutzung beschränkt. Mehr Info.",
-	},
-    },
-login_page:
-	en: {
+		},
+  },
+	login_page: {
+		en: {
 	    title: "Login",
 	    username: "Username",
 	    password: "Enter Password",
@@ -40,8 +41,8 @@ login_page:
 	    forgot_password: "Forgot Password?",
 	    error1: "Username is required",
 	    error2: "Password is required",
-	},
-        de: {
+		},
+    de: {
 	    title: "Einloggen",
 	    username: "Benutzername",
 	    password: "Passwort",
@@ -51,9 +52,9 @@ login_page:
 	    error1: "Benutzername ist erforderlich",
 	    error2: "Passwort ist erforderlich",
 	 },
-    },
-account_settings:
-	en: {
+  },
+	account_settings: {
+		en: {
 	    change_password: "Change Password",
 	    current_password: "Current Password",
 	    new_password: "New Password",
@@ -63,8 +64,8 @@ account_settings:
 	    color_theme: "Color Theme ..
 	    account: "Account",
 	    delete_account: "Delete Account",
-	},
-        de: {
+		},
+    de: {
 	    change_password: "Passwort ändern",
 	    current_password: "Aktuelles Passwort",
 	    new_password: "Neues Passwort",
@@ -74,24 +75,24 @@ account_settings:
 	    color_theme: "Farbschema-Einstellung",
 	    account: "Konto",
 	    delete_account: "Konto löschen",
- },
-    },
-server_status:
-	en: {
+ 		},
+  },
+	server_status: {
+		en: {
 	    server_status: "Server Status",
 	    response_time: "Average First Response Time",
 	    seconds: "Seconds",
 	    last_update: "Last Update",
 	    backend: "Current Translation Backend",
 	    checkmark: "Everything is working!",
-	},
-        de: {
+		},
+  	de: {
 	    server_status: "Server Status",
 	    response_time: "Durchschnittliche erste Response-Zeit",
 	    seconds: "Sekunden",
 	    last_update: "Letztes Update",
 	    backend: "Derzeitiges Übersetzungs-Backend",
 	    checkmark: "Alles funktioniert!",
-},
-    },
-
+		},
+	},
+}
