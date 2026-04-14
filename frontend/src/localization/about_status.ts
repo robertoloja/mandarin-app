717fc0574549ad5b0d61deb7c598cb1e5a8c248b
keatsgirl <svenjamg95@googlemail.com>
2026-04-14 12:32:53 +0200

    Update about_status.ts
    added localisation for privacy policy, terms of use

--- frontend/src/localization/about_status.ts
+++ frontend/src/localization/about_status.ts
@@ -5,9 +5,8 @@ const about_status = {
       de: 'Datenschutzerklärung',
     },
     content: {
-      // TODO
-      en: '',
-      de: '',
+      en: "This is a privacy preserving application. We collect no personal information or usage information, except for the user's provided e-mail address and the mandarin sentences entered into the application. User e-mail is only used to create and manage the account, no unsolicited e-mails are ever sent to the user, and this e-mail address is never shared with any third-party. Users may delete their accounts at any time, and any sentences entered into the app can be deleted at any time.",
+      de: 'Dies ist eine datenschutzbewusste Anwendung. Wir sammeln keinerlei persönliche Informationen oder Nutzerinformationen, abgesehen von der vom Nutzer freiwillig angegebenen E-Mail Adresse und den eingegebenen Sätzen in Mandarin. Die E-Mail des Nutzers wird lediglich verwendet, um ein Konto zu erstellen und zu verwalten, und wird niemals mit Dritten geteilt. Nutzer können ihr Konto jederzeit löschen; das gleiche gilt für die Inhalte, die in die Anwendung eingegeben wurden.',
     },
   },
   terms_of_use: {
@@ -16,9 +15,42 @@ const about_status = {
       de: 'Nutzungsbedingungen',
     },
     content: {
-      // TODO
-      en: '',
-      de: '',
+      intro: {
+        en: "By using this app, you agree to these Terms of Service. Please read them carefully before using the service.",
+        de: 'Mit der Nutzung dieser Anwendung akzeptieren Sie die Nutzungsbedingungen vollständig. Bitte lesen Sie diese sorgfältig, bevor Sie unseren Service in Anspruch nehmen.',
+      },
+      1: {
+        en: "Use of the Service: You may use the app only for lawful purposes and in accordance with these Terms. By creating an account, you confirm that: You are at least 13 years old. You will not misuse the app to upload harmful, illegal, or offensive content.",
+        de: 'Nutzung dieses Services: Diese Anwendung darf ausschließlich für legale und erlaubte Zwecke und in Übereinstimmung mit den Nutzungsbedingungen verwendet werden. Durch die Erstellung eines Nutzerkontos versichern Sie, dass: Sie mindestens 13 Jahre alt sind; Sie die Anwendung nicht missbrauchen, um schädliche, rechtswidrige oder anderweitig anstößige Inhalte hochzuladen.',
+      },
+      2: {
+        en: "User-Generated Content Ownership: You retain ownership of the Mandarin sentences you input into the app. Deletion: You may delete your content at any time via the app interface. Responsibility: You are solely responsible for the content you upload and its legality.",
+        de: 'Eigentumsrecht an nutzererstellten Inhalten: Die Inhalte in Mandarin, die Sie in die Anwendung eingeben, bleiben stets Ihr Eigentum. Löschung: Ihre Inhalte können jederzeit innerhalb der App von Ihnen gelöscht werden. Verantwortung: Sie tragen die Verantwortung für von Ihnen hochgeladenen Inhalte und deren Legalität, sowie für Ihre Aktivitäten in der App.',
+      },
+      3: {
+        en: "Account Responsibility: You are responsible for maintaining the confidentiality of your account credentials. You agree to notify us immediately of any unauthorized use of your account.",
+        de: 'Verantwortung für Nutzerkonto: Sie sind für die Sicherheit ihres Nutzerkontos verantwortlich und versichern, Ihre Kontoinformationen vertraulich zu behandeln. Sie stimmen zu, uns im Falle von unbefugter Verwendung ihres Kontos umgehend zu benachrichtigen.',
+      },
+      4: {
+        en: "Data Storage and Privacy: By using the app, you consent to the storage of your email and user-generated content as outlined in our Privacy Policy. We do not claim ownership of your data but reserve the right to remove content that violates these Terms.",
+        de: 'Datenspeicherung und Privatsphäre: Durch die Nutzung dieser Anwendung stimmen Sie zu, dass Ihre E-Mail Adresse und hochgeladene Inhalte wie in unserer Datenschutzerklärung beschrieben gespeichert werden. Wir erheben keinerlei Besitzansprüche auf Ihre Daten, behalten uns jedoch das Recht vor, Inhalte zu löschen, wenn diese gegen unsere Nutzungsbedingungen verstoßen.',
+      },
+      5: {
+        en: `Limitation of Liability: MandoBot is provided on an "as-is" and "as-available" basis. We make no warranties of any kind regarding the service's reliability, availability, or suitability for your purposes. To the extent permitted by law, we disclaim all liability for damages resulting from your use of the app.`,
+        de: 'Haftungsausschuss: mandoBot wird ohne Gewähr und Anspruch auf Vollständigkeit bereitgestellt. Wir bemühen uns um Aktualität und Vollständigkeit der Anwendung, gewährleisten jedoch nicht, dass unser Service stets korrekt, abrufbar oder für Ihre Zwecke passend ist. Soweit gesetzlich zulässig schließen wir die Haftung für jegliche Schäden aus, welche aus der Nutzung unserer Anwendung resultieren.',
+      },
+      6: {
+        en: "Termination: We reserve the right to suspend or terminate your account if you violate these Terms.",
+        de: 'Kündigung von Konten: Wir behalten uns vor, Ihr Konto zu sperren oder zu löschen, falls Sie gegen die hier vorliegenden Bedingungen verstoßen.',
+      },
+      7: {
+        en: "Changes to These: Terms We may update these Terms from time to time. Significant changes will be communicated via email or through the app.",
+        de: 'Änderungen der Nutzungsbedingungen: Es ist möglich, dass wir diese Bedingungen mitunter aktualisieren. Erhebliche Änderungen werden über E-Mail oder durch die App mitgeteilt.',
+      },
+      8: {
+        en: "Contact: For questions or concerns about these Terms, contact us at mandobotserver@gmail.com",
+        de: 'Kontakt: Bei Fragen oder Anliegen zu diesen Bedingungen kontaktieren Sie uns bitte unter mandobotserver@gmail.com',
+      },
     },
   },
   support_this_project: {
@@ -27,9 +59,9 @@ const about_status = {
       de: 'Unterstütze dieses Projekt',
     },
     content: {
-      // TODO
-      en: '',
-      de: '',
+      intro: {
+        en: "If mandoBot is useful to you, please consider supporting the project with a monthly subscription. Doing so provides several benefits:",
+        de: 'Wenn mandoBot hilfreich für Sie ist, ziehen Sie doch in Erwägung, dieses Projekt mit einem monatlichen Abo zu unterstützen. Dies hat einige Vorteile:',
     },
   },
   server_status: {
@@ -42,7 +74,7 @@ const about_status = {
       de: 'Durchschnittliche erste Response-Zeit',
     },
     seconds: {
-      en: 'Seconds',
+      en: 'seconds',
       de: 'Sekunden',
     },
     last_update: {
