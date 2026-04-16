ee76bd2f2b7e02acc734d928925c075b466bb1a9
Roberto Loja <robertoloja@gmail.com>
2026-04-16 16:16:25 +0200

    Frontend: Add strings to be localized

---
+++ frontend/src/localization/registration_page.ts
@@ -0,0 +1,49 @@
+const registration_page = {
+  toasts: {
+    account_created: {
+      title: {
+        en: 'Account created!',
+        de: '',
+      },
+      description: {
+        en: 'Redirecting you to the user preferences page',
+        de: '',
+      },
+    }
+  },
+  heading: {
+    en: 'Register',
+    de: '',
+  },
+  email: {
+    en: 'E-mail',
+    de: '',
+  },
+  username: {
+    en: 'Username',
+    de: '',
+  },
+  password: {
+    en: 'Password',
+    de: '',
+  },
+  register: {
+    en: 'Register',
+    de: '',
+  },
+  user_already_exists: {
+    en: 'A user with this e-mail already exists.',
+    de: '',
+  },
+  forgot_password: {
+    1: {
+      en: 'If you have forgotten your password,',
+      de: '',
+    },
+    link: {
+      en: 'click here to reset it',
+      de: '',}
+  }
+};
+
+export default registration_page;
