6513f908d9fd923ac2c2f20878356e5aeac82dff
keatsgirl <svenjamg95@googlemail.com>
2026-05-06 19:25:42 +0200

    homepage translations

--- frontend/src/localization/home_page.ts
+++ frontend/src/localization/home_page.ts
@@ -7,48 +7,45 @@ const home_page = {
     en: 'By breaking down Chinese sentences into constituent words, mandoBot helps beginners better understand structure, vocabulary, and meaning. \n\n**Enter a Mandarin sentence into the textbox above, press _Submit_, and the sentence will be broken down into constituent words!** \n\nWe also offer a curated [reading room](/reading) featuring pre-translated and segmented classical texts, making complex material more accessible to anyone with an interest in Mandarin.',
     de: 'Indem mandoBot chinesische Sätze in ihre einzelnen Wörter zerlegt, hilft die App besonders Anfängern, Struktur, Wortschatz und Bedeutung besser zu verstehen. \n\n**Geben Sie einen Satz in Mandarin in das Textfeld oben ein, klicken sie auf _Absenden_, und der Satz wird in seine einzelnen Wörter zerlegt!** \n\nAußerdem bieten wir einen kuratierten [Lesesaal](/reading) mit bereits übersetzen und segmentierten klassischen Texten an, wodurch komplexes Material für alle, die sich für Mandarin interessieren, leichter zugänglich gemacht wird.',
   },
-  welcome_returning: {
-    en: 'Lorem ipsum dolor sit amet. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    de: 'Lorem ipsum dolor sit amet. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  },
+ 
   news_heading: {
     en: 'Development News',
-    de: '',
+    de: 'Aktuelles',
   },
   news_items: [
     {
       heading: {
         en: 'German Translations',
-        de: '',
+        de: 'Deutsche Übersetzungen',
       },
       date: 'May 6th, 2026',
       body: {
         en: "We're extremely happy to announce that mandoBot has been _fully_ localized into German, thanks to the amazing efforts of our newest team member [Svenja M.](mailto:svenja@mandobot.com)! This includes all the [Reading Room](/reading) texts, as well as the full segmentation/translation functions. \n\n On any page, you may switch the application's language from English to German by clicking on the button on the top-right (next to the color mode button), and selecting your prefered language. \n\n Welcome to any and all German-speaking users, and feel free to [let us know](mailto:mistakes@mandobot.com) if we've made any translation mistakes.",
-        de: '',
+        de: 'Wir freuen uns sehr, euch mitteilen zu können, dass mandoBot dank des großartigen Einsatzes unseres neuesten Teammitglieds [Svenja M.](mailto:svenja@mandobot.com) nun _vollständig_ ins Deutsche lokalisiert wurde! Dies umfasst alle Texte im [Lesesaal](/reading), sowie die gesamten Segmentierungs- und Übersetzungsoptionen. \n\n Auf jeder Seite kann die Sprache der Anwendung von Englisch auf Deutsch umgestellt werden, indem man auf die Schaltfläche oben rechts (neben der Schaltfläche für den Farbmodus) klickt und seine bevorzugte Sprache auswählt. \n\n Herzlich willkommen an alle deutschsprachigen Nutzer! Bitte [lasst uns wissen](mailto:mistakes@mandobot.com), falls uns Übersetzungsfehler unterlaufen sind.',
       },
       author: 'mandoBot team',
     },
     {
       heading: {
         en: 'New Domain Name',
-        de: '',
+        de: 'Neuer Domainname',
       },
       date: 'May 3rd, 2026',
       body: {
         en: 'MandoBot finally has a permanent home on the web, at [www.mandobot.com](/). All previous links should still work with the old URL. Finally, members of the mandoBot team now have @mandobot.com email addresses.',
-        de: '',
+        de: 'MandoBot hat endlich eine feste Adresse im Internet: [www.mandobot.com](/). Alle bisherigen Links sollten weiterhin über die alte URL funktionieren. Außerdem verfügen die Mitglieder des mandoBot Teams nun über E-Mail Adressen mit der Endung @mandobot.com.',
       },
       author: 'mandoBot team',
     },
     {
       heading: {
         en: "...and we're back!",
-        de: '',
+        de: '...und wir sind wieder da!',
       },
       date: 'May 1st, 2026',
       body: {
         en: 'After a period of inactivity, mandoBot is now actively being improved again. In the following months, you can expect new languages, new reading room texts, and new features to speed up all of our Mandarin learning journeys. As always, the core features of mandoBot will remain free for all users, while paying users will have access to better translations (DeepL instead of Argos), more reading room texts, and more personalized features. \n\n Watch this space for upcoming announcements!',
-        de: '',
+        de: 'Nach einer Phase der Inaktivität wird mandoBot nun wieder aktiv weiterentwickelt. In den kommenden Monaten könnt ihr euch auf neue Sprachen, neue Texte im Lesesaal und neue Funktionen freuen, die unsere Mandarin-Lernprozesse beschleunigen werden. Wie immer bleiben die Kernfunktionen von mandoBot für alle Nutzer kostenlos, während zahlende Nutzer Zugang zu besseren Übersetzungen (DeepL statt Argos), mehr Texten im Lesesaal und personalisierten Funktionen erhalten. \n\n Behaltet diese Seite für künftige Ankündigungen im Auge!',
       },
       author: 'mandoBot team',
     },
